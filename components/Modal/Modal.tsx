e15c5b2df314ffa1af8388d003b56aa1734b5a1a
Sang Tran <sang.tran@jobhopin.com>
2022-03-27 15:04:23 +0700

    add modal comp

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal: React.FC<{}> = ({ children }) => {
+  const el = useRef(document.createElement("div"));
+  
+  
+  useEffect(() => {
+    const modalRoot = document.querySelector("#modal-root")
+    const current = el.current;
+
+    modalRoot!.appendChild(current);
+    return () => void modalRoot!.removeChild(current);
+  }, []);
+
+  return createPortal(children, el.current);
+};
+
+export default Modal;
